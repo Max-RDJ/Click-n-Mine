691af5bab9da56f2f32cb55d8a7af884b9c29fbc
Max-RDJ <maxjeremiahgd@gmail.com>
2024-11-11 16:43:44 +0000

    Started adding functionality for furnaces

--- Project/index.js
+++ Project/index.js
@@ -56,7 +56,7 @@ let miningRate = 1;
 let countStone = 0;
 let countCopper = 0;
 let countTin = 0;
-let countmotherlode = 100;
+let countBronze = 0;
 
 function updateDisplay(element, count) {
   element.innerHTML = count;
@@ -76,6 +76,8 @@ const countermotherlodeDisplay = document.querySelector('#motherlode-count');
 stoneNode.addEventListener("click", () => {
   countStone += 1 * miningRate;
   updateDisplay(counterStoneDisplay, countStone);
+  completeObjective("stone10");
+  completeObjective("stone30");
   updateInfoMessage("You mine some stone.");
 });
 
@@ -84,6 +86,7 @@ copperNode.addEventListener("click", () => {
     countCopper += 0.5 * miningRate;
     updateDisplay(counterCopperDisplay, countCopper);
     updateInfoMessage("You mine some copper.");
+    completeObjective("copperAndTin");
   } else {
     updateInfoMessage("You need a pickaxe.");
   }
@@ -94,18 +97,95 @@ tinNode.addEventListener("click", () => {
     countTin += 0.5 * miningRate;
     updateDisplay(counterTinDisplay, countTin);
     updateInfoMessage("You mine some tin.");
+    completeObjective("copperAndTin");
   } else {
     updateInfoMessage("You need a pickaxe.");
   }
 });
 
-motherlodeNode.addEventListener("click", () => {
-  if (countmotherlode > 0) {
-    countmotherlode -= 1 * miningRate;
-    updateDisplay(countermotherlodeDisplay, countmotherlode);
-    updateInfoMessage("You chip away at the motherlode.");
+let objective = [
+  {
+    id: "dummy",
+    message: "You shouldn't be seeing this.",
+    condition: "None.",
+    complete: true,
+    unlock: "none"
+  },
+  {
+    id: "stone10",
+    message: "Mine 10 stone by clicking on the node in the Mineshaft.",
+    condition: (countStone) => countStone >= 10,
+    complete: false,
+  },
+  {
+    id: "stone30",
+    message: "Mine 30 stone.",
+    condition: (countStone) => countStone >= 30,
+    complete: false
+  },
+  {
+    id: "sellAll",
+    message: "Click on the stone icon under Resources and then click 'Sell all'.",
+    condition: () => countCoins >= 30,
+    complete: false,
+    unlock: () => {
+      $("#tools").css("display", "block");
+    }
+  },
+  {
+    id: "buyPickaxe",
+    message: "Now, buy yourself a pickaxe from the Shop by clicking on it.",
+    condition: () => hasPickaxe == true,
+    complete: false,
+    unlock: () => {
+      $("#copper-node").css("display", "block");
+      $("#tin-node").css("display", "block");
+    }
+  },
+  {
+    id: "copperAndTin",
+    message: "Mine some copper and tin.",
+    condition: () => countCopper > 0 && countTin > 0,
+    complete: false,
+    unlock: () => {
+      $("#furnaces").css("display", "block");
+    }
+  },
+  {
+    id: "smeltIngot",
+    message: "Once you have enough money, purchase your first Furnace and select Bronze in the drop-down menu.",
+    condition: () => $("#ingotSelection") = "Bronze",
+    unlock: () => {
+      // Display ingot counts under Resources
+    }
   }
-});
+]
+
+let objectiveMessage = document.getElementById("objective-message");
+
+
+function updateObjectiveMessage() {
+  const activeObjective = objective.find(obj => !obj.complete);
+  if (activeObjective) {
+    objectiveMessage.innerHTML = activeObjective.message;
+  } else {
+    objectiveMessage.innerHTML = "All objectives completed!";
+  }
+}
+
+updateObjectiveMessage();
+
+function completeObjective(objectiveId) {
+  const obj = objective.find(obj => obj.id === objectiveId);
+
+  if (obj && obj.condition(countStone, countCopper, countTin)) {
+    obj.complete = true;
+    updateObjectiveMessage();
+    if (obj.unlock) obj.unlock();
+  }
+}
+
+
 
 // Pickaxes
 let hasPickaxe = false;
@@ -139,6 +219,7 @@ const element = document.getElementById(id);
     updateCoinsDisplay();
     updateInfoMessage(`You buy a ${type} pickaxe.`);
     hasPickaxe = true;
+    completeObjective("buyPickaxe");
   } else if (element.style.opacity === "1") {
     updateInfoMessage("You've already bought that.");
   } else {
@@ -148,14 +229,14 @@ const element = document.getElementById(id);
 
 
 // Purchasing auto-miners
-const autoMiners = [
-  { id: "human-miner-1", cost: 500, autoMiningRate: 1, ore: counterStoneDisplay, oreCounter: countStone },
-  { id: "human-miner-2", cost: 500,autoMiningRate: 1, ore: counterStoneDisplay, oreCounter: countStone },
-  { id: "human-miner-3", cost: 500,autoMiningRate: 1, ore: counterStoneDisplay, oreCounter: countStone }
+const autoMinerList = [
+  { id: "human-miner-1", cost: 500, autoMiningRate: 1 },
+  { id: "human-miner-2", cost: 500, autoMiningRate: 1 },
+  { id: "human-miner-3", cost: 500, autoMiningRate: 1 }
 ] 
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  autoMiners.forEach(({ id, cost, autoMiningRate, ore, oreCounter }) => {
+  autoMinerList.forEach(({ id, cost, autoMiningRate, ore, oreCounter }) => {
     document.getElementById(id).addEventListener("click", () => buyMiner(id, cost, autoMiningRate, ore, oreCounter));
   });
 })
@@ -175,13 +256,6 @@ function buyMiner(id, cost, autoMiningRate) {
     updateInfoMessage("You buy a miner.");
 
     startMining();
-
-    // const intervalDuration = 3000 / currentAutoMiningRate;
-
-    // element.autoMiningInterval = setInterval(() => {
-    //   countStone++;
-    //   updateDisplay(ore, countStone);
-    // }, intervalDuration);
 
   } else if (countCoins < cost) {
     infoMessage = "You don't have enough coins.";
@@ -209,9 +283,9 @@ startMining();
 
 
 
-// Selling ores
-let selectedCounter;
+// Sell resources
 let selectedOre;
+let selectedCounter;
 
 function selectOre(element)
 {
@@ -226,6 +300,8 @@ $(".sellable").on("click", function()
     selectOre(this);
     selectedOre = this.id;
     selectedCounter = $(this).children("span")[0].innerHTML;
+
+    $("#sell-all").value = "Sell all " + selectedOre;
 
     console.log("Selected ore: " + selectedOre)
     console.log("Selected counter: " + selectedCounter);
@@ -278,6 +354,7 @@ $("#sell-all").on("click", function()
           updateCoinsDisplay();
           updateDisplay(counterStoneDisplay, countStone);
           updateInfoMessage("You sell all your stone.");
+          completeObjective("sellAll");
         }
         break;
       case "resource-copper":
@@ -302,6 +379,106 @@ $("#sell-all").on("click", function()
   }
 );
 
+let customAmount = document.getElementById("sell-custom-amount").value;
+
+$("#sell-custom-amount-btn").on("click", function()
+  {
+    switch(selectedOre) {
+      case "resource-stone":
+        if (countStone > 0) {
+          countCoins += customAmount;
+          countStone =- customAmount;
+          updateCoinsDisplay();
+          updateDisplay(counterStoneDisplay, countStone);
+          updateInfoMessage(`You sell ${customAmount} stone.`);
+        }
+        break;
+      case "resource-copper":
+        if (countCopper > 0) {
+          countCoins += customAmount * 2;
+          countCopper += customAmount;
+          updateCoinsDisplay();
+          updateDisplay(counterCopperDisplay, countCopper);
+          updateInfoMessage(`You sell ${customAmount} copper.`);
+        }
+        break;
+      case "resource-tin":
+        if (countTin > 0) {
+          countCoins += customAmount * 2;
+          countStone += customAmount;
+          updateCoinsDisplay();
+          updateDisplay(counterTinDisplay, countTin);
+          updateInfoMessage(`You sell ${customAmount} tin.`);
+          }
+          break;
+    }
+  }
+);
+
+
+const furnaceList = [
+  { id: "furnace1", cost: 50, smeltingRate: 1 },
+  { id: "furnace2", cost: 200, smeltingRate: 1 },
+  { id: "furnace3", cost: 500, smeltingRate: 1 },
+  { id: "furnace4", cost: 1000, smeltingRate: 1 },
+  { id: "furnace5", cost: 2500, smeltingRate: 1 },
+  { id: "furnace6", cost: 5000, smeltingRate: 1 },
+  { id: "furnace7", cost: 8000, smeltingRate: 1 },
+  { id: "furnace8", cost: 10000, smeltingRate: 1 },
+] 
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  furnaces.forEach(({ id, cost, smeltingRate, ingotCounter }) => {
+    document.getElementById(id).addEventListener("click", () => buyFurnace(id, cost, smeltingRate, ingotCounter));
+  });
+})
+
+
+function buyFurnace(id, cost, smeltingRate, selectedIngot) {
+  const element = document.getElementById(id);
+  
+  if (element.style.opacity !== "1" && countCoins >= cost) {
+    element.style.opacity = "1";
+    countCoins -= cost;
+    currentSmeltingRate += smeltingRate;
+
+    updateCoinsDisplay();
+    updateInfoMessage("You buy a furnace.");
+
+  } else if (countCoins < cost) {
+    infoMessage = "You don't have enough coins.";
+
+  } else {
+    infoMessage = "You've already bought that.";
+  }
+}
+
+let currentSmeltingRate = 0;
+let smeltingIntervalId = null;
+const counterBronzeDisplay = document.getElementById("bronze-count");
+
+function startSmelting(selectedIngot) {
+  if (smeltingIntervalId) {
+    clearInterval(smeltingIntervalId);
+  }
+
+  if (currentSmeltingRate > 0) {
+    const intervalDuration = 3000 / currentSmeltingRate;
+    miningIntervalId = setInterval(() => {
+      selectedIngot++;
+      updateDisplay(counterBronzeDisplay, selectedIngot);
+    }, intervalDuration);
+  }
+}
+
+let selectedIngot = document.getElementById("ingotSelection").value;
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("ingot-selection").addEventListener("change", () => startSmelting(selectedIngot));
+  });
+
 
 // Temporarily increase scale of coins and its icon when number changes
 // function coinBounce()
@@ -323,7 +500,6 @@ $("#sell-all").on("click", function()
 //     }, 100);
 // }
 
-
 // Persist resource counts and purchases
 function savePlayerProgress() {
   localStorage.setItem("countCoins", JSON.stringify({countCoins}));
@@ -331,7 +507,13 @@ function savePlayerProgress() {
 }
 
 // Dev tools
+$("#reset-money").click(function() {
+  countCoins = 0;
+  updateCoinsDisplay();
+});
+
 $("#mo-money").click(function() {
   countCoins += 1000;
   updateCoinsDisplay();
 });
+
